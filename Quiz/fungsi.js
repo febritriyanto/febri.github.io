fe4efd14b740bbfb53090b5a392fc555f3b048c7
febritriyanto <kenchaviciifebri@gmail.com>
2017-06-18 16:40:42 +0700

    Add files via upload

---
+++ Quiz/fungsi.js
@@ -0,0 +1,95 @@
+
+//materi
+function fungsiklik()
+{
+	 document.getElementById('list').style.display='none';
+	 document.getElementById('materi').style.display='block';
+	 document.getElementById('latihan').style.display='none';
+	  document.getElementById('sdkelas1').style.display='none';
+	 document.getElementById('sdkelas2').style.display='none';
+	  document.getElementById('sdkelas3').style.display='none';
+
+
+	
+}
+
+function fungsiklik1sd()
+{
+	 document.getElementById('list').style.display='none';
+	 document.getElementById('sdkelas1').style.display='block';
+	 document.getElementById('sdkelas2').style.display='none';
+	  document.getElementById('sdkelas3').style.display='none';
+	 document.getElementById('latihan').style.display='none';
+
+
+	
+}
+
+function fungsiklik2sd()
+{
+	 document.getElementById('list').style.display='none';
+	 document.getElementById('materi').style.display='none';
+	 document.getElementById('latihan').style.display='none';
+	  document.getElementById('hasil').style.display='none';
+	 document.getElementById('sdkelas1').style.display='none';
+	 document.getElementById('sdkelas2').style.display='block';
+	 document.getElementById('sdkelas3').style.display='none';
+
+	
+}
+
+function fungsiklik3sd()
+{
+	 document.getElementById('list').style.display='none';
+	 document.getElementById('materi').style.display='none';
+	 document.getElementById('latihan').style.display='none';
+	  document.getElementById('hasil').style.display='none';
+	 document.getElementById('sdkelas1').style.display='none';
+	 document.getElementById('sdkelas2').style.display='none';
+	 document.getElementById('sdkelas3').style.display='block';
+
+	
+}
+
+
+//latihan
+function fungsiklikl()
+{
+	 document.getElementById('list').style.display='none';
+	 document.getElementById('latihan').style.display='block';
+	 document.getElementById('materi').style.display='none';
+	 document.getElementById('hasil').style.display='none';
+	 document.getElementById('sdkelas1').style.display='none';
+	 document.getElementById('sdkelas2').style.display='none';
+	 document.getElementById('sdkelas3').style.display='none';
+	
+}
+
+//kembali
+function kembali()
+{
+	 document.getElementById('list').style.display='block';
+	 document.getElementById('materi').style.display='none';
+	 document.getElementById('latihan').style.display='none';
+	 document.getElementById('hasil').style.display='none';
+	 document.getElementById('sdkelas1').style.display='none';
+	 document.getElementById('sdkelas2').style.display='none';
+	 document.getElementById('sdkelas3').style.display='none';
+	 
+	
+}
+
+//hasil
+function hasil()
+{
+	 document.getElementById('list').style.display='none';
+	 document.getElementById('materi').style.display='none';
+	 document.getElementById('latihan').style.display='none';
+	 document.getElementById('hasil').style.display='block';
+	 document.getElementById('sdkelas1').style.display='none';
+	 document.getElementById('sdkelas2').style.display='none';
+	 document.getElementById('sdkelas3').style.display='none';
+	
+}
+
+//latihan
